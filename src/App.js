5dd42b50c2b4f839ce683809655ff347eadfaa96
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-04-15 23:34:22 +0300

    Add data extraction and action dispatch feature inside App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import './index.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import { useEffect } from 'react';
+import Modal from './components/Modal';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems('random'));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-    </div>
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
-
 export default App;
